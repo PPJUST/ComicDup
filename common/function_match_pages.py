2229f35f7100ebdbbc0a39e583a700546f1cbb84
PPJUST <106465946+ppjust@users.noreply.github.com>
2025-12-29 20:52:52 +0800

    优化：两本漫画之间对应的相似页的方法

--- common/function_match_pages.py
+++ common/function_match_pages.py
@@ -1,11 +1,5 @@
 # 相似页匹配
 
-"""
-1.传入两个comicinfo类
-2.计算comicinfo类中所有图片的hash
-3.已页数多的comicinfo类为基础，逐个读取其页码图片hash，与另一个comicinfo类的所有页码进行匹配，如果识别为相似图片，则写入字典变量
-4.完成全部读取后，整理字典列表，返回一个能表示两个info类页码相似对应关系的字典
-"""
 from typing import Dict, List
 
 import lzytools_image
@@ -13,8 +7,9 @@ import lzytools_image
 from common.class_comic import ComicInfoBase
 
 
-def match_pages(comic_info_1: ComicInfoBase, comic_info_2: ComicInfoBase):
-    """匹配两本漫画相似的页码"""
+def match_pages(comic_info_1: ComicInfoBase, comic_info_2: ComicInfoBase) -> Dict[str, List[str]]:
+    """匹配两本漫画相似的页码
+    :return: 字典，键为第一本漫画的内部页路径，值为对应的相似的第二本漫画的内部页路径"""
     # 计算ComicInfo类中所有图片的hash值
     # 计算第一个ComicInfo
     images_hash_1 = comic_info_1.calc_hashs()
@@ -30,3 +25,5 @@ def match_pages(comic_info_1: ComicInfoBase, comic_info_2: ComicInfoBase):
                 if page_1 not in similar_pages:
                     similar_pages[page_1] = []
                 similar_pages[page_1].append(page_2)
+
+    return similar_pages
